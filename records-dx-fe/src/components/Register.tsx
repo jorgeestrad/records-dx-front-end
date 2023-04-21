0bafa70efe6414c0a1d49848a1aefbe256e45271
Jorge Enrique Estrada Correa <jorgeestradacorrea@gmail.com>
2023-04-21 11:34:38 -0500

    we

--- records-dx-fe/src/components/Register.tsx
+++ records-dx-fe/src/components/Register.tsx
@@ -20,46 +20,28 @@ interface SignUpProps {
     }
  }
 
- type resultProps = {
-    email: string;
-    gender: string;
-  };
-
- const Regex = RegExp(/^\s?[A-Z0–9]+[A-Z0–9._+-]{0,}@[A-Z0–9._+-]+\.[A-Z0–9]{2,4}\s?$/i);
-
+ 
  const Register = () => {
-   
-   const [validated, setValidated] = useState(false); 
+      const [validated, setValidated] = useState(false); 
       const [statusMessage, setStatusMessage] = useState("TBD");
       const [userName, setUserName] = useState('');
-        
    
-    const handleChange = (event : any) => {
-      event.preventDefault();
-           
-     
-    }
-    
-    const handleSubmit = (event : any) => {
-      const form = event.currentTarget;
-      
-      if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-       
-        alert(validated);
-      }else
-      {
-         alert(validated);
-         fetchData();
-      }
-      setValidated(true);
-      
-    };
+      const handleSubmit = (event : any) => {
+         const form = event.currentTarget;
+         if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+            alert(validated);
+         }else
+         {
+            alert(validated);
+            fetchData();
+         }
+         setValidated(true);
+      };
    
      const fetchData = () => {
-
-      if (validated && userName.length > 0 ){
+       if (validated && userName.length > 0 ){
          fetch("https://api.weather.gov/")
          .then((response) => response.json())
          .then((response) => {
@@ -68,7 +50,7 @@ interface SignUpProps {
          .catch(() => {
             setStatusMessage("ERROR");
          });
-      }
+       }
     };
    
     return(
@@ -76,12 +58,13 @@ interface SignUpProps {
       <div className='wrapper'>
             <div className='form-wrapper'>
                <h2>Registro de usuario</h2>
-               <div><hr /><br /></div>
+               <div><hr /></div>
                <Form noValidate validated={validated} onSubmit={handleSubmit}>
                <Form.Group as={Col} md="12" controlId="validationCustom01">
                   <div className='username'>
                      <label htmlFor="username">Nombre del usuario</label>
-                     <Form.Control type='text'
+                     <Form.Control 
+                        type='text'
                         id='username' name='username'
                         size="sm"
                         placeholder="Ingrese su alias/nombre de usuario"
@@ -106,6 +89,14 @@ interface SignUpProps {
                         id='password' name='password'
                         size="sm"
                         placeholder="Ingrese su contraseña"
+                        required />
+                  </div>
+                  <div className='password'>
+                     <label htmlFor="password">Confirme su Contraseña</label>
+                     <Form.Control type='password'
+                        id='passwordConfirm' name='passwordConfirm'
+                        size="sm"
+                        placeholder="Confirme su contraseña"
                         required />
                   </div>
                   <div>API Status: {statusMessage}</div>
